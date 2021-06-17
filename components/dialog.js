aeba31a3bb7958f9723429f57eb59037f4e41326
xaota <1172619+xaota@users.noreply.github.com>
2021-06-17 23:24:50 +0500

    move dependency to toolbox

--- components/dialog.js
+++ components/dialog.js
@@ -1,6 +1,6 @@
 import Component, { html, css } from '../script/Component.js';
 import { updateChildrenText } from '../script/DOM.js';
-import Deferred from 'javascript-std-lib/deferred.js';
+import Deferred from 'javascript-toolbox/library/Deferred.js';
 import UIButton  from './button.js';
 // eslint-disable-next-line no-unused-vars
 import UICaption from './caption.js';
