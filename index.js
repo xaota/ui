1e457d34558a2b51b9caafa39376d31f4140f045
xaota <1172619+xaota@users.noreply.github.com>
2022-01-11 20:17:43 +0530

    router moved into toolbox, removed Defer (todo - check it)

--- index.js
+++ index.js
@@ -3,5 +3,4 @@ import Component from './script/Component.js';
 export * from './script/Template.js';
 export * from './script/DOM.js';
 export { default as $ } from './script/DOM.js';
-export { default as Router } from './script/Router.js';
 export default Component;
